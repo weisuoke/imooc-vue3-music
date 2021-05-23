a1574cb3adaed04d58250aa5d60287d05374447c
梅林的法杖 <530264127@qq.com>
2021-05-23 22:49:15 +0800

    2.4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <m-header></m-header>
+  <Tab />
+  <router-view></router-view>
 </template>
 
 <script>
 import MHeader from '@/components/header/header'
+import Tab from '@/components/tab/tab'
 
 export default {
   components: {
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
