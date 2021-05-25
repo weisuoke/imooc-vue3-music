efe403a7ed3ab7035b8b4eb592594003e12bd5b9
梅林的法杖 <530264127@qq.com>
2021-05-26 06:16:41 +0800

    3.2

---
+++ src/components/index-list/IndexList.vue
@@ -0,0 +1,122 @@
+<template>
+  <scroll class="index-list">
+    <ul>
+      <li
+        v-for="group in data"
+        :key="group.title"
+        class="group"
+      >
+        <h2 class="title">{{group.title}}</h2>
+        <ul>
+          <li
+            v-for="item in group.list"
+            :key="item.id"
+            class="item"
+          >
+            <img class="avatar" v-lazy="item.pic" />
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script>
+import Scroll from '@/components/base/scroll/scroll'
+
+export default {
+  name: 'IndexList',
+  props: {
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  components: {
+    Scroll
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.index-list {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: $color-background;
+
+  .group {
+    padding-bottom: 30px;
+
+    .title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background-color: $color-highlight-background;
+    }
+
+    .item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+
+  .fixed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+
+    .fixed-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background-color: $color-highlight-background;
+    }
+  }
+
+  .shortcut {
+    position: absolute;
+    right: 8px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 22px;
+    padding: 20px 0;
+    border-radius: 10px;
+    text-align: center;
+    background-color: $color-background-d;
+    font-family: Helvetica; // 英文字体推荐使用
+    .item {
+      padding: 4px;
+      line-height: 1;
+      color: $color-text-l;
+      font-size: $font-size-small;
+
+      &.current {
+        color: $color-theme;
+      }
+    }
+  }
+}
+</style>
